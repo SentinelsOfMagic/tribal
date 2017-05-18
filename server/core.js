8ee889458b83f034a4bef4702a08512fe7a0b2d7
kvo180 <khoavo180@gmail.com>
2017-05-17 17:01:41 -0700

    Change 'PlayList to Playlist' for consistency

--- server/core.js
+++ server/core.js
@@ -97,7 +97,7 @@ io.on( 'connection', function(client) {
     if ( playlistId ) {
       console.log( `Client requesting playlist ${playlistId}` );
 
-      p = db.getSinglePlayList( playlistId )
+      p = db.getSinglePlaylist( playlistId )
         .then( (doc) => {
           if ( !doc ) {
             throw new Error( 'playListNotFound' );
@@ -107,13 +107,13 @@ io.on( 'connection', function(client) {
         .catch(
           (err) => (err.message === 'playListNotFound' ),
           () => {
-            return db.createPlayList();
+            return db.createPlaylist();
           });
 
     } else {
 
       console.log( 'Client requesting new playlist' );
-      p = db.createPlayList();
+      p = db.createPlaylist();
     }
 
     p.then( (doc) => {
