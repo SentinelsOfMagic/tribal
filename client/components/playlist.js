b9405b83c39f6884f0b9921ffebc093188c4bcda
April Ablon <ablonapril@gmail.com>
2017-05-20 15:59:09 -0700

    Add socket functionality and call database on refresh to get playlist status

--- client/components/playlist.js
+++ client/components/playlist.js
@@ -2,34 +2,52 @@ angular.module('tribal')
 
 .controller('PlaylistController', function($location, tribalServer) {
   this.playlistHash = $location.search().playlist;
-  this.partying = false;
-  this.playing = true;
+  tribalServer.getPlaylist(this.playlistHash)
+    .then(res => {
+      console.log('Successful getPlaylist call');
+      this.partying = res.data.started;
+      this.playing = res.data.playing;
+    })
+    .catch(err => console.log('Error in getting playlist'));
+  // this.partying = false;
+  // this.playing = true;
   tribalServer.getCurrentSong(this.playlistHash)
     .then(res => {
+      console.log('get currentSong success');
       this.currentSong = res.data;
     })
     .catch(err => console.log('Error in setting currentSong'));
   this.startParty = ($event) => {
     console.log('startParty');
-    this.partying = true;
     tribalServer.startParty(this.playlistHash);
   };
   this.clickPlay = ($event) => {
     console.log('clickPlay');
-    tribalServer.playSong(this.playlistHash);
+    tribalServer.resumeSong(this.playlistHash);
   };
   this.clickPause = ($event) => {
     console.log('clickPause');
     tribalServer.pauseSong(this.playlistHash);
   };
+  this.handleStartParty = () => {
+    console.log('startParty');
+    this.partying = true;
+    this.playing = true;
+    console.log('partying: ', this.partying);
+    console.log('playing: ', this.playing);
+  };
   this.handlePlay = () => {
     console.log('handlePlay');
     this.playing = true;
+    this.partying = true;
+    console.log('playing: ', this.playing);
   };
   this.handlePause = () => {
     console.log('handlePause');
     this.playing = false;
+    console.log('playing: ', this.playing);
   };
+  tribalServer.registerStartParty(this.handleStartParty);
   tribalServer.registerPlay(this.handlePlay);
   tribalServer.registerPause(this.handlePause);
 })
