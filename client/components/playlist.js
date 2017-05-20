0709fab7dfd77c4aa1910f40b8330b3d56920845
April Ablon <ablonapril@gmail.com>
2017-05-20 11:29:20 -0700

    Conditionally rendering currently playing song using Spotify API

--- client/components/playlist.js
+++ client/components/playlist.js
@@ -3,22 +3,12 @@ angular.module('tribal')
 .controller('PlaylistController', function($location, tribalServer) {
   this.playlistHash = $location.search().playlist;
   this.partying = false;
-  // this.currentSong = false;
-  this.currentSong = {
-    item: {
-      album: {
-        images: [{}, {
-          url: 'https://i.scdn.co/image/5ca023498e951d2bdcc2b8a4b6e88717fa9b6e1f'
-        }, {
-          url: 'https://i.scdn.co/image/d866cccb797f489ca1f0046ed750d140c3ff6ca3'
-        }]
-      },
-      name: 'Bonfire',
-      artists: [{
-        name: 'Childish Gambino'
-      }]
-    }
-  };
+  this.playing = true;
+  tribalServer.getCurrentSong(this.playlistHash)
+    .then(res => {
+      this.currentSong = res.data;
+    })
+    .catch(err => console.log('Error in setting currentSong'));
   this.startParty = ($event) => {
     console.log('startParty');
     this.partying = true;
